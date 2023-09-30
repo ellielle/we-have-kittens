5b4606481046a36195ebc3ac704f8abdf71ec086
ellielle <40385743+ellielle@users.noreply.github.com>
2023-09-30 10:29:26 -0400

    add page load animation

---
+++ src/lib/ktities.ts
@@ -0,0 +1,11 @@
+// @ts-expect-error Cannot find module
+import Meow1 from "$lib/assets/images/meow1.jpg?w=250&h=250&format=webp";
+// @ts-expect-error Cannot find module
+import Meow2 from "$lib/assets/images/meow2.jpg?w=250&h=250&format=webp";
+// @ts-expect-error Cannot find module
+import Meow3 from "$lib/assets/images/meow3.jpg?w=250&h=250&format=webp";
+// import Meow1 from "$lib/assets/images/meow1.jpg?w=250&h=250&format=webp";
+// import Meow1 from "$lib/assets/images/meow1.jpg?w=250&h=250&format=webp";
+// import Meow1 from "$lib/assets/images/meow1.jpg?w=250&h=250&format=webp";
+
+export { Meow1, Meow2, Meow3 };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,38 @@
 <script>
+  import { onMount } from "svelte";
   // @ts-ignore # Cannot find module error
-  import Meow1 from "../images/meow1.jpg?w=400&h=400&format=webp";
+  import { Meow1, Meow2, Meow3 } from "$lib/ktities";
+  import { fly } from "svelte/transition";
+  import "../app.css";
+  let loaded = false;
+
+  onMount(() => {
+    loaded = true;
+  });
 </script>
 
-<h1 class="flex mt-8 text-3xl place-content-center">We have cats!</h1>
-<div class="flex flex-col mt-20 place-items-center">
-  <img class="rounded-2xl" src={Meow1} alt="A bunch of kittens!" />
-</div>
+<main class="flex flex-col place-items-center">
+  {#if loaded}
+    <h1 transition:fly={{ y: 100, duration: 2000, opacity: 0 }} class="mt-8 text-3xl text-center">
+      We have kittens!
+    </h1>
+  {/if}
+
+  {#if loaded}
+    <div transition:fly={{ y: 100, duration: 2000, opacity: 0, delay: 300 }} class="mt-4">
+      <img src={Meow1} alt="A bunch of kittens!" class="rounded-box" />
+    </div>
+  {/if}
+
+  {#if loaded}
+    <div transition:fly={{ y: 100, duration: 2000, opacity: 0, delay: 700 }} class="mt-4">
+      <img src={Meow2} alt="A bunch of kittens!" class="rounded-box" />
+    </div>
+  {/if}
+
+  {#if loaded}
+    <div transition:fly={{ y: 100, duration: 2000, opacity: 0, delay: 1200 }} class="mt-4">
+      <img src={Meow3} alt="A bunch of kittens!" class="rounded-box" />
+    </div>
+  {/if}
+</main>
