fc74b3bbaffd6b1c15a84a40aa5fc3464494fb67
ellielle <40385743+ellielle@users.noreply.github.com>
2023-09-29 21:24:29 -0400

    feat: add image optimization

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  // @ts-ignore # Cannot find module error
+  import Meow1 from "../images/meow1.jpg?w=400&h=400&format=webp";
+</script>
+
+<h1 class="flex mt-8 text-3xl place-content-center">We have cats!</h1>
+<div class="flex flex-col mt-20 place-items-center">
+  <img class="rounded-2xl" src={Meow1} alt="A bunch of kittens!" />
+</div>
