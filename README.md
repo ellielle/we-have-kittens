17f49dc95aae1780b387cf1f26086668b4057031
ellielle <40385743+ellielle@users.noreply.github.com>
2023-10-02 08:01:09 -0400

    screwing around with vercel analytics

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Kittens!
 
-A quick Svelte project I threw together to try to find homes for our kittens.
+A quick SvelteKit project I threw together to try to find homes for our kittens.
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,10 @@
+<script>
+  import { dev } from "$app/environment";
+  import { inject } from "@vercel/analytics";
+
+  inject({ mode: dev ? "development" : "production" });
+</script>
+
 <slot />
 
 <style>
