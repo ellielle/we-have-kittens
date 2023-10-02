46db16150598de5bd2aa8014ae87cf624f44288e
ellielle <40385743+ellielle@users.noreply.github.com>
2023-10-01 21:04:04 -0400

    reword the words

--- src/lib/components/CatInfo.svelte
+++ src/lib/components/CatInfo.svelte
@@ -16,6 +16,11 @@
             Only the white, orange tabby, and tuxedo kittens are available. All 3 of them are boys.
           </p>
         </li>
+        <li class="mt-2">
+          <p>
+            🎉 They were born on September 4th and will be ready to be re-homed starting October 23rd. 🎉
+          </p>
+        </li>
       </ul>
     </div>
   </div>
